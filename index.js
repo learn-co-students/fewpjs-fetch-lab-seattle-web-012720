ef071c1ef4eea91f1520b2c75bf54846c847264a
AidanLincoln <aidantlincoln@gmail.com>
2020-03-11 11:28:09 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(function(json){
+    renderBooks(json) 
+  })
 }
 
 function renderBooks(json) {
