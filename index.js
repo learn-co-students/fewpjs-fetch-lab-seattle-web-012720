9a1239d218549f831ef4a23b8374eb97b3f3874d
dianakw8591 <dianakw@gmail.com>
2020-03-11 11:17:49 -0700

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-function fetchBooks() {fetch('https://anapioficeandfire.com/api/books')
+function fetchBooks() {
+  
+  return fetch('https://anapioficeandfire.com/api/books')
 .then(function(response) {
   return response.json();
 })
